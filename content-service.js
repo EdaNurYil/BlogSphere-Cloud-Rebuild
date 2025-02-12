10c90867f9b3c1c5ea181d4d930a385d344caf87
Eda Nur Yilmaz <enyilmaz@myseneca.ca>
2025-02-12 15:28:54 -0500

    AS2 P2S123

--- content-service.js
+++ content-service.js
@@ -1,5 +1,5 @@
 //built file system 
-const file = require('fs');
+
 
 const path = reqire('path');
 
@@ -9,35 +9,9 @@ let categoried = [];
 function initialize(){
     return new Promise ((resolve, reject) =>{
         const categoriesPath = path.join(__dirname,"/categories.json");
-        const articlePath = path.join(__dirname, '/categories.json');
+        const articlePath = path.join(__dirname, '/article.json');
     })
 }
-
-file.readFile(articlePath, 'utf8' , (error,article) => {
-    if(error){
-        return reject (`Not able to read article.json: ${error}`);
-    }
-    try{
-        articles = JSON.parse(article);
-
-    }catch(parseError){
-        return reject (`Error parsing articles.json: ${parseError}`);
-
-    }
-})
-
-file.readFile(categoriesPath, 'utf8' , (error,categories) => {
-    if(error){
-        return reject (`Not able to read categories.json: ${error}`);
-    }
-    try{
-        categories = JSON.parse(categories);
-
-    }catch(parseError){
-        return reject (`Error parsing categories.json: ${parseError}`);
-
-    }
-})
 
 function getPublishedArticles() {
     return articles.filter(article => article.published);
@@ -51,4 +25,4 @@ function getCategories() {
     initialize,
     getPublishedArticles,
     getCategories
-  };
+  }
